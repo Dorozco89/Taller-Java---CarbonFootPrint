e56ed90f9525de5b23f92583028dab4a4bbfdd74
Dorozco89 <dorozco89@unisalle.edu.co>
2026-04-27 16:21:52 -0500

    Clase Coche

--- src/com/huella/modelo/Coche.java
+++ src/com/huella/modelo/Coche.java
@@ -3,12 +3,49 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.huella.modelo;
-
 /**
  *
  * @author USUARIO
  */
-public class Coche {
-    
+package com.huella.modelo;
+
+public class Coche extends FuenteEmision {
+
+    private double consumoCombustibleAnual;
+    private double factorEmision;
+
+    public Coche(String nombre, double consumoCombustibleAnual, double factorEmision) {
+        super(nombre);
+        this.consumoCombustibleAnual = consumoCombustibleAnual;
+        this.factorEmision = factorEmision;
+    }
+
+    public double getConsumoCombustibleAnual() {
+        return consumoCombustibleAnual;
+    }
+
+    public void setConsumoCombustibleAnual(double consumoCombustibleAnual) {
+        this.consumoCombustibleAnual = consumoCombustibleAnual;
+    }
+
+    public double getFactorEmision() {
+        return factorEmision;
+    }
+
+    public void setFactorEmision(double factorEmision) {
+        this.factorEmision = factorEmision;
+    }
+
+    @Override
+    public double calcularHuellaCarbono() {
+        return consumoCombustibleAnual * factorEmision;
+    }
+
+    @Override
+    public String obtenerDescripcion() {
+        return super.obtenerDescripcion()
+                + " | Tipo: Coche"
+                + " | Consumo Combustible: " + consumoCombustibleAnual
+                + " | Factor Emisión: " + factorEmision;
+    }
 }
