91f2b772f7936193f722cc1bf79de85eae98d672
Dorozco89 <dorozco89@unisalle.edu.co>
2026-04-27 15:30:41 -0500

    Clase Edificio

--- src/com/huella/modelo/Edificio.java
+++ src/com/huella/modelo/Edificio.java
@@ -3,12 +3,49 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.huella.modelo;
-
 /**
  *
  * @author USUARIO
  */
-public class Edificio {
-    
+package com.huella.modelo;
+
+public class Edificio extends FuenteEmision {
+
+    private double consumoEnergiaAnual;
+    private double factorEmision;
+
+    public Edificio(String nombre, double consumoEnergiaAnual, double factorEmision) {
+        super(nombre);
+        this.consumoEnergiaAnual = consumoEnergiaAnual;
+        this.factorEmision = factorEmision;
+    }
+
+    public double getConsumoEnergiaAnual() {
+        return consumoEnergiaAnual;
+    }
+
+    public void setConsumoEnergiaAnual(double consumoEnergiaAnual) {
+        this.consumoEnergiaAnual = consumoEnergiaAnual;
+    }
+
+    public double getFactorEmision() {
+        return factorEmision;
+    }
+
+    public void setFactorEmision(double factorEmision) {
+        this.factorEmision = factorEmision;
+    }
+
+    @Override
+    public double calcularHuellaCarbono() {
+        return consumoEnergiaAnual * factorEmision;
+    }
+
+    @Override
+    public String obtenerDescripcion() {
+        return super.obtenerDescripcion() +
+                " | Tipo: Edificio" +
+                " | Consumo Energía: " + consumoEnergiaAnual +
+                " | Factor Emisión: " + factorEmision;
+    }
 }
